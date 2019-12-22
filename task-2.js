59ca1d5f63897164ccb9c6aa1a57b959a79aa8e0
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-22 23:26:36 +0200

    task-2

---
+++ task-2.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const countProps = function(obj) {
+  return Object.entries(obj).length;
+};
+
+console.log(countProps({}));
+
+console.log(countProps({ name: "Mango", age: 2 }));
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
