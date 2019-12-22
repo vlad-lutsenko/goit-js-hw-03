cef0d18613d1aa1803a4743528a378e1210e5a17
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-23 01:14:55 +0200

    task-6

---
+++ task-6.js
@@ -0,0 +1,21 @@
+"use strict";
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 }
+];
+
+const calculateTotalPrice = function(allProdcuts, productName) {
+  let total;
+  for (const obj of allProdcuts) {
+    if (productName === obj.name) {
+      total = obj.price * obj.quantity;
+    }
+  }
+  return total;
+};
+
+console.log(calculateTotalPrice(products, "Радар")); // 5200
+
+console.log(calculateTotalPrice(products, "Дроид")); // 2800
