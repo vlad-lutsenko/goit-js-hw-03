df08ed18833caa4027fc01c22322d131359124c2
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2019-12-24 01:24:09 +0200

    t2 залишив як і було, не зрозумів як покращити код

--- task-2.js
+++ task-2.js
@@ -1,8 +1,10 @@
 "use strict";
 
 const countProps = function(obj) {
-  return Object.keys(obj).length;
+  return Object.entries(obj).length;
 };
+// була задача зробити функцію яка б повертала кількість властивостей об’єкту. саме тому (масив властивостей).ленгтс
+// не можу зрозуміти питання до ентріз. почну перебирати об’єкт - це збільшить мені кількість коду, а так - все лаконічно.
 
 console.log(countProps({}));
 
